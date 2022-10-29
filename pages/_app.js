e99f9ac9cad7f3de87b5193aeca239ca7bf54119
Shaun Smerling <shaunsmerling@gmail.com>
2022-10-29 09:37:09 +0800

    added onpage loader

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,34 @@ import React from 'react';
 import '../global.css'
 import Navbar from "../components/NavBar"
 import Script from "next/script"
-import Email from "../components/email"
+import {useRouter} from "next/router"
+import { useState, useEffect} from "react"
 
+function Loading() {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const handleStart = (url) => url !== router.asPath && setLoading(true);
+    const handleComplete = (url) => url === router.asPath && setTimeout(() => {setLoading(false)}, 5000);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+
+    return () => {
+    router.events.off("routeChangeStart", handleStart);
+    router.events.off("routeChangeComplete", handleComplete);
+    router.events.off("routeChangeError", handleComplete);
+    }
+  })
+  return loading && 
+  <div className="spinner-wrapper">
+    <div class="loadingio-spinner-spinner-xphawmzupwn"><div class="ldio-xrw7g30hrkj">
+<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div></div>
+  </div>
+}
 
 
 function MyApp({ Component, pageProps }) {
@@ -37,7 +63,7 @@ function MyApp({ Component, pageProps }) {
   }}
 />
       <Navbar/>
-     <Component {...pageProps} />
+     <><Loading/><Component {...pageProps} /></>
      
    </div>
   )
