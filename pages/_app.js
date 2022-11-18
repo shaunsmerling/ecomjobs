1b50eac525749861fbbb9340564b03bd257e0b99
shaunsmerling <shaunsmerling@gmail.com>
2022-11-18 12:02:24 +0800

    new

--- pages/_app.js
+++ pages/_app.js
@@ -14,11 +14,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div>
-     <NextSeo
-      title="Jobs For the eCommerce Industry"
-      description="Join an eCommerce brand. Help shape the future by working with some of the fastest growing Direct-To-Consumer companies." 
-    />
-      
+  
 <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-K7WC2T06Y5"/>
 <Script
   id="google-analytics"
