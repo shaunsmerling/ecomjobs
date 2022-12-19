9fd791d3a4e5de265c25e79d51ffe0b920728c9f
shaunsmerling <shaunsmerling@gmail.com>
2022-12-19 09:32:06 +0800

    updated logged in page

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import Hit from "../components/Hit";
 import { FacetDropdown } from "/components/FacetDropdown";
 import Featured from "../components/featured";
 import { NextSeo } from "next-seo";
-import Features from "../components/features"
+import {useSession} from "next-auth/react"
 
 
 
@@ -22,6 +22,9 @@ import Features from "../components/features"
 
 
 export default function HomePage() {
+
+
+  const { data: session } = useSession()
 
 
   // const {user, error, isLoading } = useUser()
@@ -50,27 +53,23 @@ export default function HomePage() {
         title="Jobs For the eCommerce Industry"
         description="Join an eCommerce brand. Help shape the future by working with some of the fastest growing Direct-To-Consumer companies."
       />
-      <HeroBanner />
-      <Featured />
-      <InstantSearch searchClient={searchClient} indexName="ecomjobs_index">
-        {/* <h2 className="mx-auto text-center text-3xl font-bold "> */}
-        {/* <div className="border-t-2 mx-24">
-        <h3 class="leading-[3rem] mt-16 mb-10 font-bold  text-left text-5xl tracking-tighter text-black max-w-2xl ">
-          Featured jobs
+      {session ?    <InstantSearch searchClient={searchClient} indexName="ecomjobs_index">
+      <h3 class="leading-[3rem] mx-auto  text-center font-bold tracking-tighter text-black text-5xl max-w-2xl ">
+        Search by Job Titles or Company Name
         </h3>
-        </div> */}
-        <h3 class="leading-[3rem] font-bold  mx-auto text-center text-4xl tracking-tighter text-black max-w-2xl ">
-          Search Job Titles or Company Names
-        </h3> */
-        <div className=" mt-12 md:grid-cols-3 divide-y md:divide-x md:divide-y-0 divide-gray-200 rounded-lg align-center drop-shadow my-10 mb-10 mx-4 lg:ml-20 lg:mr-16 ">
+   
+        <div className=" mt-12 md:grid-cols-3 divide-y md:divide-x md:divide-y-0 divide-gray-200 rounded-lg align-center drop-shadow my-10 mb-10 mx-4 lg:mx-52  ">
           <div className="rounded-lg bg-white border-2 border-black/10 overflow-hidden">
             <div className="ml-2 -mr-6">
               <div className="mt-2 mr-10 mb-2">
                 <SearchBox autofocus={true} placeholder="Search..." />
+                {/* <Search/> */}
               </div>
             </div>
+            
           </div>
         </div>
+        
 
         <div className="flex flex-wrap w-100 justify-center  mt-10 px-2 drop-shadow -  mb-10 mx-auto lg:w-max  lg:flex lg:mx-auto lg:mb-12 lg:text-center lg:drop-shadow lg:align-center ">
           <div className="mb-4 ">
@@ -108,9 +107,74 @@ export default function HomePage() {
 
         <div className="ml-2 mr-2 mb-2 -mt-2">
           <Hits hitComponent={Hit} />
+        </div> 
+   
+      </InstantSearch> : 
+      <div>
+      <HeroBanner />
+      <Featured />
+      <div className="mt-10 mb-20">
+      {/* <Step /> */}
+      </div>
+      <InstantSearch searchClient={searchClient} indexName="ecomjobs_index">
+      <h3 class="leading-[3rem] mx-auto  text-center font-bold tracking-tighter text-black text-5xl max-w-2xl ">
+        Search by Job Titles or Company Name
+        </h3>
+   
+        <div className=" mt-12 md:grid-cols-3 divide-y md:divide-x md:divide-y-0 divide-gray-200 rounded-lg align-center drop-shadow my-10 mb-10 mx-4 lg:mx-52  ">
+          <div className="rounded-lg bg-white border-2 border-black/10 overflow-hidden">
+            <div className="ml-2 -mr-6">
+              <div className="mt-2 mr-10 mb-2">
+                <SearchBox autofocus={true} placeholder="Search..." />
+                {/* <Search/> */}
+              </div>
+            </div>
+            
+          </div>
         </div>
         
+
+        <div className="flex flex-wrap w-100 justify-center  mt-10 px-2 drop-shadow -  mb-10 mx-auto lg:w-max  lg:flex lg:mx-auto lg:mb-12 lg:text-center lg:drop-shadow lg:align-center ">
+          <div className="mb-4 ">
+            <FacetDropdown
+              closeOnChange={() => window.innerWidth >= 400}
+              buttonText={({ refinements }) => {
+                return `Job Type (${refinements.length}) `;
+              }}
+            >
+              <RefinementList attribute="job_type" />
+            </FacetDropdown>
+          </div>
+          <div className="mb-4 ">
+            <FacetDropdown
+              closeOnChange={() => window.innerWidth >= 400}
+              buttonText={({ refinements }) => {
+                return `Category (${refinements.length}) `;
+              }}
+            >
+              <RefinementList attribute="job_category" />
+            </FacetDropdown>
+          </div>
+          <div className="mb-4">
+            <FacetDropdown
+              closeOnChange={() => window.innerWidth >= 400}
+              classNames={{ root: "my-LocDropdown" }}
+              buttonText={({ refinements }) => {
+                return `Location (${refinements.length}) `;
+              }}
+            >
+              <RefinementList attribute="location" />
+            </FacetDropdown>
+          </div>
+        </div>
+
+        <div className="ml-2 mr-2 mb-2 -mt-2">
+          <Hits hitComponent={Hit} />
+        </div> 
+   
       </InstantSearch>
+      </div>
+      }
     </>
   );
 }
