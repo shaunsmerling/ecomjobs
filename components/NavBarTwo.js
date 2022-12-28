fdc39e33e2f1331a2416f7b1e0303453c0c5996e
shaunsmerling <shaunsmerling@gmail.com>
2022-12-28 09:45:40 +0800

    added new jobs + images:

--- components/NavBarTwo.js
+++ components/NavBarTwo.js
@@ -35,65 +35,7 @@ export default function NavBarTwo() {
             </div>
         <div className={`-mr-20 flex`}>
             <div class="hidden lg:flex lg:items-center lg:space-x-4 lg:justify-end lg:ml-auto ">
-            <Menu as="div" className="relative inline-block text-left">
-      <div className="-ml-4">
-        <Menu.Button className="inline-flex w-full text-sm justify-center px-4 py-2 font-medium text-gray-700   ">
-        <a href="" title="" class="inline-flex items-center justify-center px-4 py-2.5 text-[12px] font-medium text-gray-900 transition-all duration-200 border border-transparent rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-ring-300">
-                    Salaries <span className="text-white z-1 bg-[#17614A] border border-black text-[10px]  ml-4 px-1  rounded-full">
-         Coming soon</span>
-                </a>
-        </Menu.Button>
-      </div>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                href=""
-                  // href="/salaries/highest-paying-ecommerce-jobs"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Highest Paying Jobs 
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                href=""
-                  // href="/salaries/lowest-paying-ecommerce-jobs"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Lowest Paying Jobs
-                </a>
-              )}
-            </Menu.Item>
            
-
-           
-          
-           
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
-          
   
     <Menu as="div" className="relative inline-block text-left">
       <div className="-ml-10">
@@ -176,6 +118,64 @@ export default function NavBarTwo() {
         </Menu.Items>
       </Transition>
     </Menu>
+    <Menu as="div" className="relative inline-block text-left">
+      <div className="-ml-8">
+        <Menu.Button className="inline-flex w-full text-sm justify-center px-4 py-2 font-medium text-gray-700   ">
+        <a href="/salaries" title="" class="inline-flex items-center justify-center px-4 py-2.5 text-[12px] font-medium text-gray-900 transition-all duration-200 border border-transparent rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-ring-300">
+                    Salaries 
+                </a>
+        </Menu.Button>
+      </div>
+
+      {/* <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      > */}
+        {/* <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1"> */}
+            {/* <Menu.Item>
+              {({ active }) => (
+                <a
+                // href=""
+                  href="/salaries/highest-paying-ecommerce-jobs"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Highest Paying Jobs 
+                </a>
+              )}
+            </Menu.Item> */}
+             {/* <Menu.Item>
+              {({ active }) => (
+                <a
+                // href=""
+                  href="/salaries/lowest-paying-ecommerce-jobs"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Lowest Paying Jobs
+                </a>
+              )}
+            </Menu.Item>
+            */}
+
+           
+          
+           
+          {/* </div> */}
+        {/* </Menu.Items>  */}
+      {/* </Transition> */}
+    </Menu>
+          
     {/* <Menu as="div" className="relative inline-block text-left"> */}
       <div className="">
         {/* <Menu.Button className="inline-flex w-full justify-center px-4 py-2 font-medium text-gray-700   "> */}
