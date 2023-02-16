538e85ac8e4cc63830187b300b287072de58a8d7
shaunsmerling <shaunsmerling@gmail.com>
2023-02-16 17:57:26 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
-    ],
+        "plugin:react/recommended",
+    ]
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
