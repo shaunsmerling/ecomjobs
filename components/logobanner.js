11d53c8ccbf9d3bf0d8ef2c8805501eacdd954e3
shaunsmerling <shaunsmerling@gmail.com>
2023-02-20 12:15:29 +0800

    hero banner styling

--- components/logobanner.js
+++ components/logobanner.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function logoBanner() {
     return (
-        <div className="mt-10 lg:mt-20 lg:-mb-20">
+        <div className="mt-20 lg:mt-20 lg:-mb-20">
              <span class="font-semibold text-gray-400 uppercase">FEATURED BRANDS</span>
             <img loading="lazy"  src="./logobanner.png" alt="logo banner" className="w-full h-12 lg:h-32 -mr-10 mt-4 object-cover" />
         </div>
