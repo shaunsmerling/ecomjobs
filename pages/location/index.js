13347e5f6b73ce8d27dc1c840b1635a4b2153da0
shaunsmerling <shaunsmerling@gmail.com>
2023-02-22 13:09:42 +0800

    check site

--- pages/location/index.js
+++ pages/location/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function index() {
     return (
-        <section class="bg-white dark:bg-gray-900">
+        <section class="bg-gray-900">
               <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
        
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Locations</h1>
