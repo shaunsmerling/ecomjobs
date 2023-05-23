c0fabf978cae92fc44895d0ecbdd93539cb8ecbe
shaunsmerling <shaunsmerling@gmail.com>
2023-05-23 16:34:59 +0800

    done

--- components/announcementBar.js
+++ components/announcementBar.js
@@ -6,12 +6,12 @@ const AnnouncementBar = () => {
   const {data: session} = useSession()
 
   return (
-    <div className="bg-black  text-white py-4 px-4 text-sm font-medium text-center">
-      <span> Stay ahead on the latest developments in artifical intelligence. </span>
+    <div className="bg-[#2d4f42]  text-white py-4 px-4 text-sm font-medium text-center">
+       <span> Stay ahead on the latest developments in artifical intelligence. </span>
 
-  <span>Subscribe to <Link href="https://thespotlight.beehiiv.com/subscribe" className="font-bold italic underline">The Spotlight</Link></span> 
- 
-    </div>
+<span>Subscribe to <Link href="https://thespotlight.beehiiv.com/subscribe" className="font-bold italic underline">The Spotlight</Link></span> 
+
+  </div>
   );
 };
 
