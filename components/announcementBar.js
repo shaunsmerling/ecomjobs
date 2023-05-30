c3827367b523880cbf8993b400e13d10dd9bb6db
shaunsmerling <shaunsmerling@gmail.com>
2023-05-31 00:06:58 +0800

    new announcement bar

--- components/announcementBar.js
+++ components/announcementBar.js
@@ -7,9 +7,9 @@ const AnnouncementBar = () => {
 
   return (
     <div className="bg-[#2d4f42]  text-white py-4 px-4 text-sm font-medium text-center">
-       <span> Stay ahead on the latest developments in artifical intelligence. </span>
+       <span> Subscribe to get over 50+ new jobs sent directly to your inbox weekly! </span>
 
-<span>Subscribe to <Link href="https://thespotlight.beehiiv.com/subscribe" className="font-bold italic underline">The Spotlight</Link></span> 
+
 
   </div>
   );
