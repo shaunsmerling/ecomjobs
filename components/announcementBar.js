ab0c988396e3237812ca3ea7a85fe74120eac128
shaunsmerling <shaunsmerling@gmail.com>
2023-06-17 20:08:30 +0800

    ads

--- components/announcementBar.js
+++ components/announcementBar.js
@@ -7,7 +7,7 @@ const AnnouncementBar = () => {
 
   return (
     <div className="bg-[#2d4f42]  text-white py-4 px-4 text-sm font-medium text-center">
-       <span> Subscribe to get over 50+ new jobs sent directly to your inbox weekly! </span>
+       <span> I'm sorry about the ads. I'll turn them off when we make more money 😔 </span>
 
 
 
